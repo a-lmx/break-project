aef1f582e30643a0b207fd6d62016de3101949e0
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-07 12:16:46 -0700

    creating headers for Language model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seattle_zip_codes = [98177, 98133, 98125, 98117, 98107, 98103, 98115, 98105, 
+  98195, 98112, 98102, 98199, 98119, 98109, 98122, 98121, 98101, 98164, 98174, 
+  98104, 98144, 98134, 98136, 98126, 98116, 98106, 98118]
+
+# To make Language records
+# Loop thru zipcode rows, build hashes for each language
+
+languages = [
+HD01_VD01,Estimate; Total:
+HD01_VD02,Estimate; Total: - Speak only English
+HD01_VD03,Estimate; Total: - Spanish or Spanish Creole:
+HD01_VD06,"Estimate; Total: - French (incl. Patois, Cajun):"
+HD01_VD09,Estimate; Total: - French Creole:
+HD01_VD12,Estimate; Total: - Italian:
+HD01_VD15,Estimate; Total: - Portuguese or Portuguese Creole:
+HD01_VD18,Estimate; Total: - German:
+HD01_VD21,Estimate; Total: - Yiddish:
+HD01_VD24,Estimate; Total: - Other West Germanic languages:
+HD01_VD27,Estimate; Total: - Scandinavian languages:
+HD01_VD30,Estimate; Total: - Greek:
+HD01_VD33,Estimate; Total: - Russian:
+HD01_VD36,Estimate; Total: - Polish:
+HD01_VD39,Estimate; Total: - Serbo-Croatian:
+HD01_VD42,Estimate; Total: - Other Slavic languages:
+HD01_VD45,Estimate; Total: - Armenian:
+HD01_VD48,Estimate; Total: - Persian:
+HD01_VD51,Estimate; Total: - Gujarati:
+HD01_VD54,Estimate; Total: - Hindi:
+HD01_VD57,Estimate; Total: - Urdu:
+HD01_VD60,Estimate; Total: - Other Indic languages:
+HD01_VD63,Estimate; Total: - Other Indo-European languages:
+HD01_VD66,Estimate; Total: - Chinese:
+HD01_VD69,Estimate; Total: - Japanese:
+HD01_VD72,Estimate; Total: - Korean:
+HD01_VD75,"Estimate; Total: - Mon-Khmer, Cambodian:"
+HD01_VD78,Estimate; Total: - Hmong:
+HD01_VD81,Estimate; Total: - Thai:
+HD01_VD84,Estimate; Total: - Laotian:
+HD01_VD87,Estimate; Total: - Vietnamese:
+HD01_VD90,Estimate; Total: - Other Asian languages:
+HD01_VD93,Estimate; Total: - Tagalog:
+HD01_VD96,Estimate; Total: - Other Pacific Island languages:
+HD01_VD99,Estimate; Total: - Navajo:
+HD01_VD102,Estimate; Total: - Other Native North American languages:
+HD01_VD105,Estimate; Total: - Hungarian:
+HD01_VD108,Estimate; Total: - Arabic:
+HD01_VD111,Estimate; Total: - Hebrew:
+HD01_VD114,Estimate; Total: - African languages:
+HD01_VD117,Estimate; Total: - Other and unspecified languages:
+]
