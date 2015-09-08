4769fd0fff804f77a76664dd6bb99e80c25e8f9f
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-08 13:42:21 -0700

    added SEATTLE_ZIPCODES constant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  SEATTLE_ZIPCODES = ["98177", "98133", "98125", "98117", "98107", "98103", 
+    "98115", "98105", "98195", "98112", "98102", "98199", "98119", "98109", 
+    "98122", "98121", "98101", "98164", "98174", "98104", "98144", "98134", 
+    "98136", "98126", "98116", "98106", "98118"]
 end
