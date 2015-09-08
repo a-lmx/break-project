1a54ea6488d4362c5e2c2d4f3a4b2d9a0f7d41e8
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-08 13:41:58 -0700

    added by_city scope

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,2 +1,4 @@
 class Zipcode < ActiveRecord::Base
+  # SCOPES ---------------------------------------------------------------------
+  scope :by_city, ->(list_of_zipcodes) { Zipcode.where(zipcode: list_of_zipcodes) }
 end
