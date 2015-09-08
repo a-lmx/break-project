fa3cfea95c426ed786e962164a84c5857c64702d
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-08 13:41:29 -0700

    moved seattle_zipcodes to ApplicationController

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,10 +57,6 @@ CSV.foreach("db/aff_download/ACS_13_5YR_B16001_with_ann.csv",
   Zipcode.create(zipcode_hash)
 end
 
-# seattle_zipcodes = [98177, 98133, 98125, 98117, 98107, 98103, 98115, 98105, 
-#   98195, 98112, 98102, 98199, 98119, 98109, 98122, 98121, 98101, 98164, 98174, 
-#   98104, 98144, 98134, 98136, 98126, 98116, 98106, 98118]
-
 # Store density in Zipcode model ?
 # => calculate in the model
 
