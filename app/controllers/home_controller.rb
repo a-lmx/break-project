aeb30a21153dc551559f26444f01c5c46d5a082c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-08 14:25:03 -0700

    added vancouver data to ApplicationController and index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @seattle_zips = Zipcode.by_city(ApplicationController::SEATTLE_ZIPCODES)
+    @vancouver_zips = Zipcode.by_city(ApplicationController::VANCOUVER_ZIPCODES)
     @language = params[:language]
   end
 end
