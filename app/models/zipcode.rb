8516a9e1dced388a1c592c9e7c8d7760d2cb2474
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-13 22:04:46 -0700

    added space for formatting

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,4 +1,4 @@
 class Zipcode < ActiveRecord::Base
   # SCOPES ---------------------------------------------------------------------
-  scope :by_city, ->(list_of_zipcodes) { Zipcode.where(zipcode: list_of_zipcodes) }
+  scope :by_city, -> (list_of_zipcodes) { Zipcode.where(zipcode: list_of_zipcodes) }
 end
