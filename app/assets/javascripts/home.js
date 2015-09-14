3a3d9f5767446be1f94d19b9bafd8f1e93c6639a
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-13 20:30:17 -0700

    removed sample code

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,47 +5,4 @@ $(function() {
     var language = $("#language").val();
 
     var url = "/" + language;
-
-  //   if (url) {
-  //     $.ajax(url, {
-  //       type: "GET",
-  //       success: function(data, textStatus, jqHXR) {
-  //         if (jqHXR.status == 204) {
-  //           apologize();
-  //         } else {
-  //           var songs = data;
-  //           displayMessage(message);
-  //           displaySongs(songs);
-  //         }
-  //       }
-  //     });
-  //   } else { // no search term entered
-  //     apologize();
-  //   }
-  //   clearTextField(); // after the results have rendered
-  // });
-
-  // function connectApiCalltoButton(className, message) {
-  //   var element = "form." + className;
-  //   var url = "/" + className;
-
-  //   $(element).submit(function(event) {
-  //   event.preventDefault();
-
-  //   clearTextField(); // as soon as the button is clicked
-  //   removeOldResults();
-
-  //   $.ajax(url, {
-  //       type: "GET",
-  //       success: function(data) {
-  //         var songs = data;
-  //         displayMessage(message);
-  //         displaySongs(songs);
-  //       }
-  //     });
-  // });
-  // }
-
-  // connectApiCalltoButton("popular", "Follow the herd, listen to these jams:");
-
 });
